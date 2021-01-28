5423044c27dafe844e826f216b6feda8b7c1002d
harisprakoso <47579268+harisprakoso@users.noreply.github.com>
2021-01-28 21:10:26 +0700

    Update upload.php

--- upload.php
+++ upload.php
@@ -5,7 +5,7 @@ $filetmp  = $_FILES['file']['tmp_name'];
  
 echo "<form method='POST' enctype='multipart/form-data'>
         <input type='file'name='file' />
-        <input type='submit' value='go' />
+        <input type='submit' value='APLOD COK' />
  
 </form>";
  
